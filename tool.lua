9eb11237bf51cd498ae2ad82ce6badd936f72265
Jerry Hong <513498646@qq.com>
2014-08-20 15:28:07 +0800

    updating

--- tool.lua
+++ tool.lua
@@ -9,17 +9,15 @@ function tool:table2string(t)
     local sep = ""
     for k,v in pairs(t)do
         if type(k) == 'number' then 
-            str = str .. sep   .. '[' .. self:table2string(k) .. ']' .. "=" ..  self:table2string(v) 
-            sep = ","
+            str = str .. sep   .. '[' .. self:table2string(k) .. ']' .. " = " ..  self:table2string(v) 
+            sep = ", "
         else 
-            str = str .. sep   ..'"'.. self:table2string(k)..'"' .. "=" ..  self:table2string(v) 
-            sep = ","
+            str = str .. sep   ..'"'.. self:table2string(k)..'"' .. " = " ..  self:table2string(v) 
+            sep = ", "
         end
     end
     return '{' .. str ..'}'
 end
 
---return tool
+return tool
 
-s = {a =3,4,{s=1,b=3}}
-print(tool:table2string(s))
