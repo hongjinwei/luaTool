2403782f48597e958eca5d6b3c80ec2a524bcdb5
Jerry Hong <513498646@qq.com>
2014-09-04 10:44:42 +0800

    update numlab

--- numlab.lua
+++ numlab.lua
@@ -8,41 +8,14 @@
 --      [2] = {2,3,4,5,6},
 --      [3] = {3,4,5,6,7}
 --}
+local cjson = require 'cjson'
 
-matlab = {}
-matlab.version = '0.0.1'
+lab = {}
+lab.version = '0.0.1'
 
---given the table{1,2,3,4,5} and return an array obj
-function matlab.array(tbl,t)
-    if not tbl or type(tbl) ~= 'table' then 
-        return nil, "not table datatype or a nil table!"
-    end
-    local array = {}
-    array.data = {}
-    for _,v in ipairs(tbl) do
-        array.data[#(array.data) + 1] = v
-    end
-    local mt = {
-        __index = {
-            T = function()  
-                    if array.datatype == 'a' then 
-                        array.datatype = 'a_t' 
-                    else 
-                        array.datatype = 'a'
-                    end
-                        return array  
-                end    
-        }
-    } 
-    setmetatable(array,mt)
-    if not t then 
-        array.datatype = "a"
-    else 
-        array.datatype = "a_t"
-    end
-    return array
-end
-
+--------------------------------------------------------------
+------ local util function
+-------------------------------------------------------------
 local function array_len(array)
     if not string.find(array.datatype, 'a') then 
         return nil, "not a array type"
@@ -51,6 +24,15 @@ local function array_len(array)
 end
 
 local function matrix_size(matrix)
+    if type(matrix) ~= 'table' or not string.find(matrix.datatype, 'm') 
+          or not next(matrix.data) then 
+        return nil, 'not correct type'
+    end
+    
+    size = {}
+    size.len = #(matrix.data[1])
+    size.height = #(matrix.data)
+    return size
 end
 
 local function judge_cal(a, b, op)
@@ -63,8 +45,7 @@ local function judge_cal(a, b, op)
             else 
                 return true
             end
-        end
-        if string.find(a.datatype, 'm') and string.find(b.datatype, 'm') then 
+        elseif string.find(a.datatype, 'm') and string.find(b.datatype, 'm') then 
             
         end
     end
@@ -72,8 +53,152 @@ local function judge_cal(a, b, op)
 
     end
 end
+---------------------------------------------
+---------------------------------------------
+--given the table{1,2,3,4,5} and return an array obj
+function lab.array(tbl,t)
+    if not tbl or type(tbl) ~= 'table' then 
+        return nil, "not table datatype or a nil table!"
+    end
+    local array = {}
+    if not t then 
+        array.datatype = "a"
+    else 
+        array.datatype = "a_t"
+    end
+    array.data = {}
+    for _,v in ipairs(tbl) do
+        array.data[#(array.data) + 1] = v
+    end
+    local mt = {
+        __index = {
+            size = array_len(array),
+            T = function()  
+                    local mat = {}
+                    if array.datatype == 'a' then 
+                        mat.datatype = 'a_t' 
+                    else 
+                        mat.datatype = 'a'
+                    end
+                    return mat 
+                end    
+        }
+    } 
+    setmetatable(array,mt)
+    return array
+end
 
-function matlab.plus(a1, a2)
+--give table like  {{1,2,3},{4,5,6}} create a matrix obj
+function lab.matrix(tbl)
+    if type(tbl) ~= 'table' or type(tbl[1]) ~= 'table' then 
+        return nil, 'not correct argument'
+    end
+    
+    if not next(tbl[1]) then 
+        return nil, 'empty table'
+    end
+
+    len = #(tbl[1])
+    for _,v in ipairs(tbl) do
+        if len ~= #v then 
+            return nil,'not correct argument'
+        end
+    end
+
+    matrix = {}
+    matrix.datatype = 'm'
+    matrix.data = tbl
+    matrix.size = matrix_size(matrix)
+    mt = {
+        __index = {
+            size = matrix_size(matrix),
+            T = function()
+                    mat = {}
+                    mat.data = {}
+                    mat.datatype = 'm'
+                    mat.size ={
+                        len = matrix.size.height,
+                        height = matrix.size.len
+                    }
+                    for i=1,mat.size.height do
+                        for j=1,mat.size.len do
+                            if type(mat.data[i]) ~= 'table' then 
+                                mat.data[i] = {}
+                            end
+                            mat.data[i][j] =  matrix.data[j][i]
+                        end
+                    end
+                    return mat
+                end 
+        }
+    }
+    setmetatable(matrix,mt)
+    return matrix
+end
+
+--eye function return a nxn eye matrix
+function lab.eye(n)
+    
+    if n <=0 or type(n) ~= 'number' or not tonumber(n) then 
+        return nil, 'wrong number!' 
+    end
+    local tbl = {}
+    for i=1, n do 
+        tbl[i] = {}
+        for j = 1, n do
+            if j == i then 
+                tbl[i][j] = 1 
+            else 
+                tbl[i][j] = 0
+            end
+        end
+    end
+    return lab.matrix(tbl)
+end
+
+function lab.tostring(obj)
+    if type(obj) ~= 'table' or not obj.datatype or not (string.find(obj.datatype,'a')
+        or string.find(obj.datatype,'m')) then 
+        return nil, 'error datatype'
+    end
+    if obj.datatype == 'a' then 
+        local str = 'array['
+        local sep = ''
+        for _,v in ipairs(obj.data) do
+            str = str .. sep .. v 
+            sep = ', '
+        end
+        return str .. ']\n'
+    elseif obj.datatype == 'a_t' then 
+        local str = 'array[\n'
+        local sep = ','
+        head = '        ' 
+        for k,v in ipairs(obj.data) do
+            if k == obj.size then sep = '' end
+            str = str .. head .. v .. sep .. '\n' 
+        end
+        return str .. '     ]\n'
+    elseif obj.datatype == 'm' then 
+        str = 'matrix{\n'
+        head = '        '
+        tail = ','
+        for k,v in ipairs(obj.data) do
+            inner = '['
+            sep = ''
+            if k == obj.size.height then 
+                tail = ''
+            end
+            for _,e in ipairs(v) do
+                inner = inner .. sep .. e 
+                sep = ','
+            end
+            str = str .. head .. inner .. ']' .. tail .. '\n'
+        end
+        return  str .. '      }\n'
+    end
+end
+
+function lab.plus(a1, a2)
     if a1.datatype ~= a2.datatype then 
        return  nil, "not same datatype or exists transferred array"
     end
@@ -87,11 +212,22 @@ function matlab.plus(a1, a2)
     end
 end
 
---[[
-array = matlab.array({1,2,3})
-array1 = matlab.array({4,5,6})
-s = array.T()
-a = (array1.T())
-print(s.data[2],a.datatype)
 
---]]
+array = lab.array({1,2,3})
+c = lab.array({2,3,4},'t')
+print(lab.tostring(array))
+print(lab.tostring(c))
+
+
+a = {
+    {1,2,3,4},
+    {2,3,4,6}
+    }
+
+b = lab.matrix(a)
+print(lab.tostring(b))
+d = b.T()
+print(lab.tostring(d))
+
+
+print(lab.tostring(lab.eye(4)))
